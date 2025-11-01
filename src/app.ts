89c9cd9468205f256e0956604eb0e0c171bb8bb8
mrpiper21 <benbaah104@gmail.com>
2025-11-01 09:06:57 +0000

    update

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(helmet());
 // CORS configuration
 app.use(
 	cors({
-		origin: ["http://localhost:5174", "http://localhost:5173"],
+		origin: ["http://localhost:5174", "http://localhost:5173","https://piper-client-phi.vercel.app"],
 		credentials: true,
 	})
 );
