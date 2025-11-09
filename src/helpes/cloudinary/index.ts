311939bce26326f73c5944224373d61665ff39d5
Bernard Baah <benbaah104@gmail.com>
2025-11-09 15:40:43 +0000

    Refactor dotenv import in cloudinary index file

--- src/helpes/cloudinary/index.ts
+++ src/helpes/cloudinary/index.ts
@@ -2,8 +2,6 @@
 import { v2 as cloudinary } from 'cloudinary'
 import dotenv from 'dotenv';
 dotenv.config();
-import dotenv from "dotenv";
-dotenv.config();
 
 const cloudName = process.env.CLOUDINARY_NAME;
 const apiKey = process.env.CLOUDINARY_API_KEY;
